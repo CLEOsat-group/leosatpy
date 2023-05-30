19fefe5de85251d1a06d88788404955b20d81317
chradam-py <christian.adam84@gmail.com>
2023-05-30 19:28:54 -0400

    - Updated versioning

--- utils/dataset.py
+++ utils/dataset.py
@@ -49,13 +49,13 @@ import config.base_conf as _base_conf
 
 """ Meta-info """
 __author__ = "Christian Adam"
-__copyright__ = 'Copyright 2021, UA, LEOSat observations'
-__credits__ = ["Christian Adam, Eduardo Unda-Sanzana, Jeremy Tregloan-Reed"]
-__license__ = "Free"
-__version__ = "0.1.2"
+__copyright__ = 'Copyright 2021-2023, CLEOSat group'
+__credits__ = ["Eduardo Unda-Sanzana, Jeremy Tregloan-Reed, Christian Adam"]
+__license__ = "GPL-3.0 license"
 __maintainer__ = "Christian Adam"
 __email__ = "christian.adam84@gmail.com"
 __status__ = "Production"
+
 # -----------------------------------------------------------------------------
 # changelog
 # version 0.1.0 added first method's
